a7f1b258f6427ff86f0f395451320ec16d22b327
george-asiedu <asiedug41@gmail.com>
2025-09-24 14:13:10 +0000

    app configurations created to load env viriables

---
+++ app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str = 'development'
+    MONGO_URI: str
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8'
+    )
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
