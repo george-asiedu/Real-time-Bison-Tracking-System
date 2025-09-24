a792466ddb5d35d40becc7cf3f6ed7eed1923b1b
george-asiedu <asiedug41@gmail.com>
2025-09-24 14:43:51 +0000

    db connection initialized and app root endpoint defined

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.core.constants import messages
+from app.database.db_connection import init_db
+from app.core.logger import logger
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    logger.info(messages["db_connection"])
+
+    try:
+        yield
+    finally:
+        logger.info(messages["shutdown"])
+
+app = FastAPI(
+    title=messages["app_title"],
+    description=messages["app_description"],
+    version=messages["app_version"],
+    lifespan=lifespan,
+)
+
+@app.get("/")
+async def root():
+    return {"message": messages["welcome"]}
