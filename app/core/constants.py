594fcdd5e93576d8b19db57c4162c2642b4ad881
george-asiedu <asiedug41@gmail.com>
2025-09-25 07:07:32 +0000

    db_name_error message added

--- app/core/constants.py
+++ app/core/constants.py
@@ -5,7 +5,6 @@ messages = {
     "app_description": "A real time bison tracking system using FastAPI, beanie ODM, and MongoDB",
     "app_version": "1.0.0",
     "welcome": "Welcome to the Real-time-Bison-Tracking-System API",
-    "MONGO_DB_NAME": "Bison_Tracking",
-    "DB_ENV": "MONGO_URI",
     "db_connection_error": "DATABASE_URI is not set in environment variables",
+    "db_name_error": "DB_NAME is not set in environment variables"
 }
